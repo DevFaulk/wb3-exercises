2127a508ef3ee7b2b1b0086a874d025e658502af
Teddy Faulk <FaulkDev@gmail.com>
2023-10-24 13:36:47 -0400

    add sum practice

---
+++ functions/practice.js
@@ -0,0 +1,10 @@
+function fractionOf(a) {
+  let sum = a / 5;
+  return sum;
+
+}
+
+console.log(fractionOf(5));
+console.log(fractionOf(10));
+console.log(fractionOf(15));
+console.log(fractionOf(20));
