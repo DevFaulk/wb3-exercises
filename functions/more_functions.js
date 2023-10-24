4cdb9024b23b10720c1b10210d15b2d74de4d58f
Teddy Faulk <FaulkDev@gmail.com>
2023-10-24 11:41:21 -0400

    add receipt exercise

--- functions/more_functions.js
+++ functions/more_functions.js
@@ -1,9 +1,9 @@
+// MAILING EXAMPLE
 // let myName = "Teddy";
 // let myAddress = "123 Sesame Street";
 // let myCity = "Pittsburgh";
 // let myState = "Pennsylvania";
 // let myZipcode = "12345";
-// MAILING EXAMPLE
 // displayMailingLabel(myName, myAddress, myCity, myState, myZipcode);
 
 // function displayMailingLabel(name, address, city, state, zipcode) {
@@ -14,11 +14,29 @@
 //   console.log("Name: " + myZipcode);
 // }
 
-let num1 = 10;
-let num2 = 5;
+// ADDITION EXAMPLE
+// let num1 = 10;
+// let num2 = 5;
 
-function addNumbers(x, y) {
-  console.log(x + y);
+// function addNumbers(x, y) {
+//   console.log(x + y);
+// }
+
+// addNumbers(num2, num2);
+
+// RECEIPT EXAMPLE
+let totalDue = 100;
+let amountPaid = 60;
+let changeDue = totalDue - amountPaid;
+
+function displayReceipt(input1, input2) {
+  console.log(
+    "Total Due: $" +
+      totalDue +
+      " Amount Paid: $" +
+      amountPaid +
+      " Change Due: $" +
+      changeDue
+  );
 }
-
-addNumbers(num2, num2);
+displayReceipt(totalDue, amountPaid);
