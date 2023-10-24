e45936222208ae4eccbb4ea2b629143dc50069a2
Teddy Faulk <FaulkDev@gmail.com>
2023-10-24 10:57:12 -0400

    add more_functions exercise

---
+++ functions/more_functions.js
@@ -0,0 +1,15 @@
+let myName = "Teddy";
+let myAddress = "123 Sesame Street";
+let myCity = "Pittsburgh";
+let myState = "Pennsylvania";
+let myZipcode = "12345";
+
+displayMailingLabel(myName, myAddress, myCity, myState, myZipcode);
+
+function displayMailingLabel(name, address, city, state, zipcode) {
+  console.log("Name: " + myName);
+  console.log("Name: " + myZipcode);
+  console.log("Name: " + myCity);
+  console.log("Name: " + myState);
+  console.log("Name: " + myZipcode);
+}
