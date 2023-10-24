4931161b2aed208d92de6fbb88f0e3b7ed24224d
Teddy Faulk <FaulkDev@gmail.com>
2023-10-24 11:29:09 -0400

    add addition function

--- functions/more_functions.js
+++ functions/more_functions.js
@@ -1,15 +1,24 @@
-let myName = "Teddy";
-let myAddress = "123 Sesame Street";
-let myCity = "Pittsburgh";
-let myState = "Pennsylvania";
-let myZipcode = "12345";
+// let myName = "Teddy";
+// let myAddress = "123 Sesame Street";
+// let myCity = "Pittsburgh";
+// let myState = "Pennsylvania";
+// let myZipcode = "12345";
+// MAILING EXAMPLE
+// displayMailingLabel(myName, myAddress, myCity, myState, myZipcode);
 
-displayMailingLabel(myName, myAddress, myCity, myState, myZipcode);
+// function displayMailingLabel(name, address, city, state, zipcode) {
+//   console.log("Name: " + myName);
+//   console.log("Name: " + myZipcode);
+//   console.log("Name: " + myCity);
+//   console.log("Name: " + myState);
+//   console.log("Name: " + myZipcode);
+// }
 
-function displayMailingLabel(name, address, city, state, zipcode) {
-  console.log("Name: " + myName);
-  console.log("Name: " + myZipcode);
-  console.log("Name: " + myCity);
-  console.log("Name: " + myState);
-  console.log("Name: " + myZipcode);
+let num1 = 10;
+let num2 = 5;
+
+function addNumbers(x, y) {
+  console.log(x + y);
 }
+
+addNumbers(num2, num2);
