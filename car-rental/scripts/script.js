5635f4654ca54f323a64c0473c540fded040eabb
DevFaulk <faulkdev@gmail.com>
2023-10-30 10:30:32 -0400

    fix bugs

--- car-rental/scripts/script.js
+++ car-rental/scripts/script.js
@@ -1,51 +1,56 @@
-let pickupDate = document.getElementById("pickupDateInput");
-let rentalDays = document.getElementById("rentalDaysAmountInput").value;
-let carRentalPrice = document.getElementById("carRentalPrice");
-let optionsPrice = document.getElementById("optionsPrice");
-let ageSurcharge = document.getElementById("ageSurcharge");
-let totalAmountDue = document.getElementById("totalAmountDue");
-let optionOne = document.getElementById("optionOne");
-let optionTwo = document.getElementById("optionTwo");
-let optionThree = document.getElementById("optionThree");
+function calculateOptionTotal(rentalDays) {
+  let optionTotal = 0;
 
-let rentalDaysNumber = Number(rentalDays);
+  let optionOne = document.getElementById("optionOne");
+  let optionTwo = document.getElementById("optionTwo");
+  let optionThree = document.getElementById("optionThree");
 
-let tollTagPrice = 3.95 * rentalDaysNumber;
-let gpsPrice = 2.95 * rentalDaysNumber;
-let rsaPrice = 2.95 * rentalDaysNumber;
+  let tollTagPrice = 3.95 * rentalDays;
+  let gpsPrice = 2.95 * rentalDays;
+  let rsaPrice = 2.95 * rentalDays;
 
-let rentalPricePerDay = rentalDaysNumber * 29.99;
-
-function carOptionsCheckbox() {
   if (optionOne.checked) {
-    let optionsSelected = tollTagPrice;
-  } else if (optionTwo.checked) {
-    let optionsSelected = gpsPrice;
-  } else if (optionThree.checked) {
-    let optionsSelected = rsaPrice;
-  } else if ((optionOne.checked, optionTwo.checked)) {
-    let optionsSelected = tollTagPrice + gpsPrice;
-  } else if ((optionTwo.checked, optionThree.checked)) {
-    let optionsSelected = gpsPrice + rsaPrice;
-  } else if ((optionOne.checked, optionTwo.checked, optionThree.checked)) {
-    let optionsSelected = tollTagPrice + gpsPrice + rsaPrice;
+    optionTotal += tollTagPrice;
   }
+  if (optionTwo.checked) {
+    optionTotal += gpsPrice;
+  }
+  if (optionThree.checked) {
+    optionTotal += rsaPrice;
+  }
+
+  return optionTotal * rentalDays;
 }
 
-function underTwentyFiveToggle() {
-  if (AgeNoSelectionRadio.checked == true) {
-    let underTwentyFiveRentalPricePerDay = 0;
-    ageYesSelectionRadio.checked = false;
-  } else if (ageYesSelectionRadio.checked == false) {
-    let underTwentyFiveRentalPricePerDay =
-      rentalPricePerDay + rentalPricePerDay * 0.3;
-    AgeNoSelectionRadio.checked = false;
+function calculateAgeSurchargePrice(rentalDays) {
+  let ageSurcharge = 0;
+  let rentalPricePerDay = rentalDays * 29.99;
+
+  if (ageYesSelectionRadio.checked) {
+    ageSurcharge = rentalPricePerDay * 0.3;
+    return ageSurcharge;
+  } else {
+    return (ageSurcharge = 0);
   }
 }
 
 function estimateCostButton() {
+  let rentalDays = parseInt(
+    document.getElementById("rentalDaysAmountInput").value
+  );
+  let pickupDate = document.getElementById("pickupDateInput").value;
+  let carRentalPrice = document.getElementById("carRentalPrice");
+  let ageSurcharge = document.getElementById("ageSurcharge");
+  let totalAmountDue = document.getElementById("totalAmountDue");
+
+  let rentalPricePerDay = rentalDays * 29.99;
+  let optionTotal = calculateOptionTotal(rentalDays);
+  let ageSurchargeTotal = calculateAgeSurchargePrice(rentalDays);
+  let totalAmount = rentalPricePerDay + optionTotal + ageSurchargeTotal;
+
   carRentalPrice.innerText = rentalPricePerDay;
-  optionsPrice.innerText = optionsSelected;
-  ageSurcharge.innerText = underTwentyFiveRentalPricePerDay - rentalPricePerDay;
-  totalAmountDue.innerText = carRentalPrice + optionsPrice + ageSurcharge;
+  optionsPrice.innerText = optionTotal.toFixed(2);
+  ageSurcharge.innerText = ageSurchargeTotal.toFixed(2);
+
+  totalAmountDue = totalAmount.toFixed(2);
 }
